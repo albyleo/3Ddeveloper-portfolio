325f569bf5da40dc9a5215c7165dd527f443b6ad
albyleo <albertoleo09@outlook.it>
2024-04-08 17:32:43 +0200

    renamed:   public/nike2.glb->public/ nike_statue/nike2.glb modified:  src/components/canvas/Computers.jsx modified:  src/components/canvas/Nike2.jsx modified:  vite.config.js

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,34 +1,10 @@
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { OrbitControls, Preload } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React, { Suspense, useEffect, useState } from "react";
 
 import CanvasLoader from "../Loader";
 import Nike2 from "./Nike2";
 
-const Computers = ({ isMobile }) => {
-  const computer = useGLTF("/nike2.glb");
-
-  return (
-    <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
-      <pointLight intensity={1} />
-      <primitive
-        object={computer.scene}
-        scale={isMobile ? 0.2 : 0.25}
-        position={isMobile ? [0, -2, 3.2] : [-1, -5.25, -0.5]}
-        rotation={[-0.1, 3 , 0.1]}
-      />
-    </mesh>
-  );
-};
 
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -67,7 +43,6 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers isMobile={isMobile} />
         <Nike2/>
       </Suspense>
       
